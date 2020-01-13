3054b6fe333d3e6554dc8d588a929362914067ee
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-01-13 20:48:51 -0300

    Adiciona dados de ponderador de alunos

---
+++ R/ponderador_teste.R
@@ -0,0 +1,11 @@
+#' Base de teste para o pacote
+#'
+#' Uma base de dados para testar as funcoes do pacote
+#'
+#' @format Uma tabela com 2 colunas e 28 linhas.
+#' \describe{
+#'   \item{etapa}{etapa de ensino}
+#'   \item{ponderador}{peso do aluno por etapa}
+#' }
+#' @source Tabela provida por Todos pela educacao
+"ponderador_alunos"
