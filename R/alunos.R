6248bf4bf113c4f5cd54531bdc1ca65aa0a2ad53
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-01-13 20:48:21 -0300

    Adiciona dados de financas e de alunos

---
+++ R/alunos.R
@@ -0,0 +1,15 @@
+#' Base com dados de alunos
+#'
+#'  Base com dados de alunos de 2015 para o pacote
+#'
+#' @format Uma tabela com 6 colunas e 145264 linhas.
+#' \describe{
+#'   \item{ibge}{codigo ibge do ente federativo}
+#'   \item{nome}{nome do ente federativo}
+#'   \item{regiao}{regiao do ente federativo}
+#'   \item{estado}{estado do ente federativo}
+#'   \item{etapa}{etapa de ensino}
+#'   \item{alunos}{numero de alunos por etapa de ensino}
+#' }
+#' @source Tabela provida por Todos pela educacao
+"alunos"
---
+++ R/financas.R
@@ -0,0 +1,15 @@
+#' Base com dados de alunos
+#'
+#'  Base com dados de alunos de 2015 para o pacote
+#'
+#' @format Uma tabela com 6 colunas e 33552 linhas.
+#' \describe{
+#'   \item{ibge}{codigo ibge do ente federativo}
+#'   \item{nome}{nome do ente federativo}
+#'   \item{regiao}{regiao do ente federativo}
+#'   \item{estado}{estado do ente federativo}
+#'   \item{tipo}{tipo do fundo}
+#'   \item{valor}{valor do fundo}
+#' }
+#' @source Tabela provida por Todos pela educacao
+"financas"
