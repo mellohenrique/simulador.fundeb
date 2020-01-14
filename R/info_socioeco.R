2fd837fa669f98767c8ca6660ffcc5cdab9acfef
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-01-13 21:49:25 -0300

    Adiciona dados socioeconomicos

---
+++ R/info_socioeco.R
@@ -0,0 +1,15 @@
+#' Base com dados de valor socioeconomicos
+#'
+#'  Base com dados de alunos de 2015 para o pacote
+#'
+#' @format Uma tabela com 6 colunas e 5592 linhas.
+#' \describe{
+#'   \item{ibge}{codigo ibge do ente federativo}
+#'   \item{nome}{nome do ente federativo}
+#'   \item{regiao}{regiao do ente federativo}
+#'   \item{estado}{estado do ente federativo}
+#'   \item{nse}{valor de indicador de nivel socioeconomico}
+#'   \item{idhm}{valor de indice de desenvolvimento humano municipal}
+#' }
+#' @source Tabela provida por Todos pela educacao
+"info_socioeco"
