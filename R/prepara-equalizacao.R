4e22bb6fde7b925a717adcff2fac496a510e2e88
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-01-19 16:10:03 -0300

    Altera nomes do parametro dos alunos avaliado

--- R/prepara-equalizacao.R
+++ R/prepara-equalizacao.R
@@ -4,7 +4,7 @@
 #'
 #' @param dados data.frame que sera preparado para equalizacao
 #' @param var_vaa variavel de valor as ser equalizada entre entes federativos
-#' @param var_alunos_grupos coluna numerica com o numero de alunos por ente
+#' @param var_alunos_avaliada coluna numerica com o numero de alunos por ente
 #'
 #' @return Um data.frame com as seguintes colunas adicionas cumulativo de alunos e gasto necessario para equalizar
 #'
@@ -12,11 +12,11 @@
 #' @examples
 #' library(simulador.fundeb)
 
-prepara_equalizacao <- function(dados, var_vaa = vaa, var_alunos_grupo = alunos_estado){
+prepara_equalizacao <- function(dados, var_vaa = vaa, var_alunos_avaliada = alunos_estado){
   dados %>%
     dplyr::arrange({{var_vaa}}) %>%
     dplyr::mutate(
-      cumulativo_alunos = cumsum({{var_alunos_grupo}}),
-      necessario_equalizacao = ({{var_vaa}} * cumulativo_alunos) - cumsum({{var_vaa}} * {{var_alunos_grupo}})
+      cumulativo_alunos = cumsum({{var_alunos_avaliada}}),
+      necessario_equalizacao = ({{var_vaa}} * cumulativo_alunos) - cumsum({{var_vaa}} * {{var_alunos_avaliada}})
     )
   }
