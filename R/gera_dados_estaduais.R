add887d1983bf8346b9e644d57e0c1ac83812719
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-01-22 21:55:29 -0300

    Cria funcao gera dados estaduais

---
+++ R/gera_dados_estaduais.R
@@ -0,0 +1,23 @@
+#' @title Gera tabela com dados dos estados
+#'
+#' @description Função intermediária que gera dados dos estados que será usada nas simulações
+#'
+#' @param dados data.frame preparado por pondera_geral
+#'
+#' @return Um data.frame com as seguintes colunas adicionas cumulativo de alunos e gasto necessario para equalizar
+#'
+#' @importFrom magrittr %>%
+#' @examples
+#' library(simulador.fundeb)
+#'
+#' <- dplyr::left_join(
+
+gera_dados_estaduais <- function(dados){
+  dplyr::left_join(
+    calcula_alunos_estadual(dados),
+    calcula_alunos_estadual(dados, var_alunos = alunos_socioeco_fin) %>%
+      dplyr::rename(alunos_estado_socio = alunos_estado)) %>%
+      dplyr::left_join(calcula_fundo_estadual(dados)) %>%
+      dplyr::mutate(vaa = fundo_estadual / alunos_estado,
+                    vaa_socio = fundo_estadual / alunos_estado_socio)
+  }
