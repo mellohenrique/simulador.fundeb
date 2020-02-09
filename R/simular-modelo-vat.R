e09388c3891f746ad51f832a03015c5e8b4f790a
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-02-09 11:48:19 -0300

    Padroniza nome do parametro de equalizacao

--- R/simular-modelo-vat.R
+++ R/simular-modelo-vat.R
@@ -7,8 +7,8 @@
 #' @param base_socioeconomica data.frame com  o codigo do ibge, o numero de alunos ponderado e uma variavel socioeconomia a ponderar
 #' @param base_financas data.frame com o codigo do ibge e os valores do fundeb e das demais receitas
 #' @param auxilio_federal percentual do fundo que a União complementará
-#' @param var_fundo parametro com o nome da variavel do fundo a ser considerado
-#' @param var_alunos parametro com o nome da variavel alunos a ser considerada
+#' @param equalizacao_socio parametro lógico que controla se a equalização do fundo considerara o vetor de alunos ou de alunos socioeconomico
+#' @param distribuicao_fundo_estadual_socio parametro logico que controla se a distribuicao do fundo estadual considerara o vetor de alunos ou de alunos socioeconomico
 #'
 #' @return Data.frame com alunos ponderador por ente federativo
 #'
@@ -17,7 +17,7 @@
 #' @export
 #'
 
-simular_modelo_vat <- function(base_alunos, ponderador, base_socioeconomica, base_financas, auxilio_federal = 0.1, var_fundo = fundeb, var_alunos = alunos, distribuicao_fundo_estadual_socio = FALSE, equalizacao_vaa = FALSE,...){
+simular_modelo_vat <- function(base_alunos, ponderador, base_socioeconomica, base_financas, auxilio_federal = 0.1, var_fundo = fundeb, var_alunos = alunos, distribuicao_fundo_estadual_socio = FALSE, equalizacao_socio = FALSE,...){
   dados <- pondera_geral(base_alunos, ponderador_alunos, base_socioeconomica, base_financas)
   dados_estaduais <- gera_dados_estaduais(dados)
   aporte_federal <- auxilio_federal * calcula_fundo_total(dados)
@@ -38,7 +38,7 @@ simular_modelo_vat <- function(base_alunos, ponderador, base_socioeconomica, bas
           distribuicao_fundo_estadual_socio ~ recursos_totais / alunos_socioeco,
           TRUE ~ recursos_totais / alunos))
 
-    if(equalizacao_vaa){
+    if(equalizacao_socio){
       fundo_equalizado <- equaliza_modelo(dados, fundo = recursos_totais, aporte = aporte_federal, var_alunos = alunos_socio, codigo = ibge) %>%
         dplyr::rename(recursos_complementados = fundo_equalizado)
       dados <- dplyr::left_join(dados, fundo_equalizado) %>%
