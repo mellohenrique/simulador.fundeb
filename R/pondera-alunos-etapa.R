934008bd52d1ae41d730a3053a77d0c39ea7496a
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-02-25 16:24:25 -0300

    Altera a documentacao da funcao pondera_alunos_etapa, remove variavel nao usada dos parametros da funcao e atualiza a documentacao

--- R/pondera-alunos-etapa.R
+++ R/pondera-alunos-etapa.R
@@ -1,12 +1,12 @@
 #' @title Pondera alunos por etapa de ensino
 #'
-#' @description Recebe uma base com código ibge, etapa e alunos e outra com pesos de ponderador por etapa e gera uma tabela com o número de alunos ponderados por entidade da federação
+#' @description Recebe uma base com codigo ibge, etapa e alunos e outra com pesos de ponderador por etapa e gera uma tabela com o numero de alunos ponderados por etapa para cada entidade do grupo considerado
 #'
 #' @param base_alunos data.frame de numero de alunos por etapa e ente federativo
 #' @param ponderador data.frame de peso de aluno por etapa
-#' @param codigo coluna numerica com o codigo do estado
-#' @param etapa_ensino coluna de fator ou caractere com a etapa de ensino
-#' @param var_alunos coluna numerica com o numero de alunos
+#' @param codigo coluna numerica com o codigo do ente federativo ou estado
+#' @param var_alunos nomeia a coluna numerica com o numero de alunos que sera utilizada
+#' @param variavel_peso nomeia coluna que sera utilizada como peso da etapa de ensino
 #'
 #' @return Data.frame com alunos ponderador por ente federativo
 #'
@@ -15,7 +15,7 @@
 #' library(simulador.fundeb)
 
 
-pondera_alunos_etapa <- function(base_alunos, ponderador, codigo = ibge, etapa_ensino = etapa, var_alunos = alunos, variavel_peso = peso){
+pondera_alunos_etapa <- function(base_alunos, ponderador, codigo = ibge, var_alunos = alunos, variavel_peso = peso){
   base_alunos %>%
     dplyr::left_join(ponderador) %>%
     dplyr::group_by({{codigo}}) %>%
