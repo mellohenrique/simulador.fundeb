faee8e440d78a3f320df79ca265c7fe5d8cc2abf
Henrique de Assunção <henrique.mello1992@gmail.com>
2024-03-02 20:12:04 -0300

    transforma a funcao em uma multiplicacao matricial

--- R/pondera-alunos-etapa.R
+++ R/pondera-alunos-etapa.R
@@ -10,42 +10,16 @@
 #'
 
 
-pondera_alunos_etapa <- function(dados_fnde, peso_etapas = peso, difere_etapas_complementacao = c("vaaf_vaat", "mesmos_pesos"), produto_dt = TRUE){
-  # Binding variables para NULL
-  alunos = alunos_ponderados = . = uf = ibge = NULL
+pondera_alunos_etapa <- function(dados_alunos, dados_peso){
 
-  difere_etapas_complementacao = match.arg(difere_etapas_complementacao)
-  dados_fnde = checa_transforma_dt(dados_fnde)
+  matriz_alunms = as.matrix(matriculas[order(matriculas$ibge), !names(matriculas) %in% c('ibge')])
+  alunos_vaaf = as.matrix(matriculas[, pesos$etapa]) %*% pesos$peso_vaaf
+  alunos_vaat = as.matrix(matriculas[, pesos$etapa]) %*% pesos$peso_vaat
 
-  suppressWarnings({
-  dados_fnde[,`:=`(nome = NULL)]
-  })
+  df_alunos = data.frame(ibge = matriculas$ibge[order(matriculas$ibge)],
+             alunos_vaaf = alunos_vaaf,
+             alunos_vaat = alunos_vaat)
 
-  alunos_tidy = suppressWarnings(melt(dados_fnde, id.vars = c("uf", "ibge"),
-                                      variable.name = "etapa",
-                                      value.name = "alunos",
-                                      variable.factor = FALSE,
-                                      verbose = FALSE))
-
-  alunos_tidy[, `:=`(alunos = ifelse(is.na(alunos), 0, alunos))]
-
-  if (difere_etapas_complementacao == "vaaf_vaat"){
-    alunos_tidy[peso_etapas, `:=`(peso_vaaf = peso_vaaf,
-                                  peso_vaat = peso_vaat), on = "etapa"]
-  } else if (difere_etapas_complementacao == "mesmos_pesos") {
-
-    alunos_tidy[peso_etapas, `:=`(peso_vaaf = peso,
-                                  peso_vaat = peso), on = "etapa"]
-  }
-
-  alunos_tidy[,
-              `:=`(alunos_vaaf = alunos * peso_vaaf,
-                   alunos_vaat = alunos * peso_vaat)]
-
-  alunos_tidy = alunos_tidy[,.(alunos = sum(alunos),
-                               alunos_vaaf = sum(alunos_vaaf),
-                 alunos_vaat = sum(alunos_vaat)), by = .(uf, ibge)]
-
-  return(retorna_dt_df(alunos_tidy, produto_dt = produto_dt))
+  return(df_alunos)
 
 }
