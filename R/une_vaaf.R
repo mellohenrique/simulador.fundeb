d0589358b855d4432b4d205743817e527e9d2234
Henrique de Assunção <henrique.mello1992@gmail.com>
2024-03-04 22:28:53 -0300

    Altera une vaaf para novo formato

--- R/une_vaaf.R
+++ R/une_vaaf.R
@@ -3,24 +3,28 @@
 #' @description Une a equalização do fundo etapa vaaf com a tabela dos entes e calcula o valor aluno ano da etapa
 #'
 #' @inheritParams simula_fundeb
-#' @param dados_entes data.frame ou data.table com os dados dos entes federativos
-#' @param dados_etapa data.frame ou data.table com os dados da equalizacao da etapa
+#' @param df_entes data.frame com os dados dos entes federativos
+#' @param df_estados data.frame com os dados dos fundos estaduais
+#' @param df_fundos_estaduais data.frame com os dados da equalizacao da etapa dos fundos estaduais
 #'
 #' @return Um data.frame ou data.table com a simulacao dos dados do FNDE
 
-une_vaaf <- function(dados_entes,
-                     dados_etapa){
+une_vaaf <- function(df_entes,
+                     df_estados,
+                     df_fundos_estaduais){
 
-  dados_entes[dados_etapa,
-              `:=`(fundo_estadual_vaaf = receitas_etapa,
-                   equalizacao_vaaf = equalizacao),
-              on = .(uf)]
+  # Une bases
+  df = merge(df_entes, df_estados[, c('uf', 'alunos_estado_vaaf')], all.x = TRUE, by = 'uf')
+  df = merge(df, df_fundos_estaduais, all.x = TRUE, by = 'uf')
 
+  # Calcula valores
+  df$recursos_vaaf_final = df$alunos_vaaf * df$recursos_pos / df$alunos_estado_vaaf
+  df$vaaf_final = df$recursos_vaaf_final / df$alunos_vaaf
 
-   dados_entes[,`:=`(
-     vaaf = fundo_estadual_vaaf/sum(alunos_vaaf),
-     fundo_vaaf = fundo_estadual_vaaf*alunos_vaaf/sum(alunos_vaaf)),
-                by = .(uf)]
+  # Remove colunas indesejadas
+  df$alunos_estado_vaaf  = NULL
+  df$recursos_pos = NULL
 
-  return(dados_entes)
+  # Retorna resultados
+  return(df)
 }
