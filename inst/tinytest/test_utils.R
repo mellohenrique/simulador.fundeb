6cafc72273c6b6a93fd9494ed6e99c5618d39436
Henrique de Mello Santos de Assuncao <henrique.assuncao@sesicni.com.br>
2024-06-26 13:26:07 -0300

    Corrige teste

--- inst/tinytest/test_utils.R
+++ inst/tinytest/test_utils.R
@@ -10,7 +10,6 @@ expect_equal(simulador.fundeb:::reescala_vetor(1:11, minimo =  1, maximo = 1.5),
 expect_error(simulador.fundeb:::checa_numerico('a', 'a'))
 expect_error(simulador.fundeb:::checa_data_frame('a', 'a'))
 expect_error(simulador.fundeb:::checa_na(c('a', NA_character_), 'a'))
-
 expect_true(simulador.fundeb:::checa_numerico(1, 'a'))
-expect_true(simulador.fundeb:::checa_data_frame(simulador.fundeb::teste_matriculas, 'a'))
+expect_true(simulador.fundeb:::checa_data_frame(simulador.fundeb:::teste_matriculas, 'a'))
 expect_true(simulador.fundeb:::checa_na(c('a', 'b'), 'a'))
